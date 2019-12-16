4c90f7a8efb29cf2b2731acaf633980d5128bde0
Mikalai <mikalai.h@smartum.pro>
2019-12-16 23:47:48 +0300

    test styled component

--- src/form.jsx
+++ src/form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export class Form extends React.Component {
     constructor(props){
@@ -9,13 +10,29 @@ export class Form extends React.Component {
             this.props.onChangeField({name, value});
     };
 
+    formStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        backgroundColor: '#666566',
+        width: '100%',
+        height: '300px'
+    };
+
     render() {
         return (
-            <form>
-                <input onChange={({target}) => this.changeField('first', target.value)}/>
-                <input onChange={({target}) => this.changeField('second', target.value)}/>
-                <input onChange={({target}) => this.changeField('third', target.value)}/>
+            <form style={this.formStyle}>
+                <Input onChange={({target}) => this.changeField('first', target.value)}/>
+                <Input onChange={({target}) => this.changeField('second', target.value)}/>
+                <Input onChange={({target}) => this.changeField('third', target.value)}/>
             </form>
         )
     }
 }
+
+const Input = styled.input`
+    width: 100%;
+    background-color: blue;
+    heigth: 55px;    
+`;
